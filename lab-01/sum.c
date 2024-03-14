fcb570902be1ea5edbac310e2be40edb639bb846
alan <alankalevich@gmail.com>
2024-03-14 18:14:52 -0300

    lab-01 ejercicio 2

--- lab-01/sum.c
+++ lab-01/sum.c
@@ -5,6 +5,14 @@ int main(int argc, char *argv[])
 {
     // Agregar código aquí.
 
+	int i;
+	int result = 0;
+	for(i = 1; i < argc; i++){
+		result += atoi(argv[i]);
+	}
+	
+	printf("%d\n", result);
+	
     // Termina la ejecución del programa.
     exit(EXIT_SUCCESS);
 }
