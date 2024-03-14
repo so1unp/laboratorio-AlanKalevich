fe7f57e19ac5f2be092dd3a9fbc9d834a30ae9c0
alan <alankalevich@gmail.com>
2024-03-14 18:57:00 -0300

    lab-01 ejercicio3

--- lab-01/palabras.c
+++ lab-01/palabras.c
@@ -4,7 +4,16 @@
 int main(int argc, char *argv[])
 {
     // Agregar código aquí.
+	
+	int letra;
+	while((letra = getchar()) != EOF){
+		printf("%c", letra);
+		if(letra == ' '){
+			printf("\n");
+		}
+	}
 
+	
     // Termina la ejecución del programa.
     exit(EXIT_SUCCESS);
 }
